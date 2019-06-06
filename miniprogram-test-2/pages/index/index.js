2958401deddb8ee229ad5fa2bdcfa5cfdd237730
havendeng <dhf949209210@qq.com>
2019-06-06 19:33:43 +0800

    通知跑马灯效果完成，但是用了太多的js动画看是否有优化空间

--- miniprogram-test-2/pages/index/index.js
+++ miniprogram-test-2/pages/index/index.js
@@ -6,24 +6,27 @@
 // 因为这个时候的this.setData里面的副本还是没有更新前的。
 /* jshint esversion: 6 */
 const app = getApp();
-
 Page({
   data: {
-    InformTheContent: [
-      // {id:1,content:"你好我是"},
-      // {id:2,content:"我根本不"}
+    informTheContent: [
+      {id: 1,content: "点进度条,狂点矿车"},
+      {id: 2,content: "我根本不想知道你在说什么"},
       {
-        id: 1,
-        content: "点进度条,狂点矿车"
+        id: 3,
+        content: "只能用于数组ARRAY取值，对key,value只有这样取值才是正确的"
       },
       {
-        // id: 2,
-        // content: "我根本不想知道你在说什么"
+        id: 4,
+        content: "小程序JS循环取值"
+      },
+      {
+        id: 5,
+        content: "pdf下载地址：Java面试宝典 第一章内容介绍	20 第"
       }
     ],
 
     orientation: "left",
-    marqueeDistance: 0,
+    marqueeDistance: 320,
     scrollFontSize: 24,
     getPrizeTipsShow: true,
     // 在此定义一个全局的timer，用于金币下落一段时间后减速
@@ -34,6 +37,7 @@ Page({
     coinFallSpeed: 2,
     // 金币下落的加速度
     coinFallAcceleratedSpeed: 1,
+    scrollTextWidth: null,
     collectGoldCoin: null,
     barOutViewWidth: null,
     containerHeight: null,
@@ -48,16 +52,16 @@ Page({
       desc: '分享页面的内容',
       path: '/pages/index/index' // 路径，.传递参数到指定页面。
     };
-
   },
-    getUserInfo: function (e) {
-      console.log(e);
-      app.globalData.userInfo = e.detail.userInfo;
-      this.setData({
-        userInfo: e.detail.userInfo,
-        hasUserInfo: true
-      });
-    },
+
+  getUserInfo: function (e) {
+    console.log(e);
+    app.globalData.userInfo = e.detail.userInfo;
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    });
+  },
   onLoad: function () {
     let barOutViewWidth = parseInt((app.globalData.ownsCoinNum / app.globalData.coinTotalNum) * 490);
     this.setData({
@@ -72,7 +76,7 @@ Page({
           fallCoinTop: this.data.fallCoinTop + Math.ceil(this.data.coinFallSpeed * this.data.coinFallAcceleratedSpeed),
           coinFallAcceleratedSpeed: this.data.coinFallAcceleratedSpeed + 0.1,
         });
-        
+
         // console.log(this.data.fallCoinTop);
       } else {
         this.setData({
@@ -81,8 +85,32 @@ Page({
         });
       }
     }, 30);
+    // 以下是为了计算文字跑马灯所在是view 的长度
+    let widthCount = 0;
+    for (let i = 0; i < this.data.informTheContent.length; i++) {
+
+      widthCount = (this.data.informTheContent[i].content.length * this.data.scrollFontSize) + widthCount + 50;
+      console.log(widthCount);
+    }
+    this.setData({
+      scrollTextWidth: widthCount,
+    });
+
+    setInterval(() => {
+      console.log(this.data.marqueeDistance);
+      if (-(this.data.marqueeDistance) < this.data.scrollTextWidth) {
+        this.setData({
+          marqueeDistance: this.data.marqueeDistance - 13,
+        });
+        
+      } else {
+        this.setData({
+          marqueeDistance: 320,
+        });
+      }
+    }, 100);
   },
-// addCoinFallSpeed是通过点击增加金币下落速度的方法
+  // addCoinFallSpeed是通过点击增加金币下落速度的方法
   addCoinFallSpeed: function () {
     let that = this;
     clearTimeout(that.data.timer);
@@ -133,7 +161,7 @@ Page({
     // 为了在当前页面调取组件内的方法，需要先在页面内的组件节点绑定Id，获取到后再调用
     this.selectComponent("#collectGoldCoin").playCollectGoldCoinAnimat();
   },
-  onReady: function () {
+  onShow: function () {
 
   },
 
